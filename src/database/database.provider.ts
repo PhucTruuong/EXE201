491bd4f6df1689cc05ede3ccde28d6c6150b568b
PhucTruuong <133474302+PhucTruuong@users.noreply.github.com>
2024-05-17 10:50:07 +0700

    fix something

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -39,11 +39,11 @@ export const databaseProviders = [
           break;
         case PRODUCTION:
           config = {
-            username: process.env.POSTGRES_USERNAME,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DB as string,
-            host: process.env.POSTGRES_HOST as string,
-            port: process.env.POSTGRES_PORT,
+            username: process.env.POSTGRES_USERNAME_PRODUCTION,
+            password: process.env.POSTGRES_PASSWORD_PRODUCTION,
+            database: process.env.POSTGRES_DB_PRODUCTION as string,
+            host: process.env.POSTGRES_HOST_PRODUCTION as string,
+            port: parseInt(process.env.POSTGRES_PORT),
             dialect: 'postgres' as Dialect,
             dialectOptions: {
               useUTC: true
@@ -67,8 +67,9 @@ export const databaseProviders = [
       }
 
       const sequelize = new Sequelize(config);
-      console.log('NODE_ENV: ', process.env.NODE_ENV);
-      console.log('config: ', config);
+      //console.log('NODE_ENV: ', process.env.NODE_ENV);
+      //console.log('config: ', config);
+      //console.log('sequelize: ', sequelize);
       sequelize.addModels([
       ]);
       //await sequelize.sync({force: true});
