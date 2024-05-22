caae6de6965982bbff8a081821dab1a1de234b8f
AtakiSouma <atakisouma394@gmail.com>
2024-05-22 19:30:30 +0700

    init firebase

--- src/firebase/firebase.provider.ts
+++ src/firebase/firebase.provider.ts
@@ -1,17 +1,32 @@
 import { Provider } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { getStorage } from 'firebase-admin/storage';
-import * as serviceAccount from '../../petcare-6a561-firebase-adminsdk-1ctgv-f41da1d8c8.json';
+// import * as serviceAccount from '../../petcare-6a561-firebase-adminsdk-1ctgv-f41da1d8c8.json';
+import { ConfigService } from '@nestjs/config';
 
 export const firebaseProviders: Provider[] = [
     {
         provide: 'FIREBASE_ADMIN',
-        useFactory: () => {
-            const app = admin.initializeApp({
-                credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+            const firebaseConfig = {
+                type: configService.get<string>('TYPE'),
+                project_id: configService.get<string>('PROJECT_ID'),
+                private_key_id: configService.get<string>('PRIVATE_KEY_ID'),
+                private_key: configService.get<string>('PRIVATE_KEY').replace(/\\n/g, '\n'),
+                client_email: configService.get<string>('CLIENT_EMAIL'),
+                client_id: configService.get<string>('CLIENT_ID'),
+                auth_uri: configService.get<string>('AUTH_URI'),
+                token_uri: configService.get<string>('TOKEN_URI'),
+                auth_provider_x509_cert_url: configService.get<string>('AUTH_CERT_URL'),
+                client_x509_cert_url: configService.get<string>('CLIENT_CERT_URL'),
+                universe_domain: configService.get<string>('UNIVERSAL_DOMAIN'),
+            } as admin.ServiceAccount
+            return admin.initializeApp({
+                credential: admin.credential.cert(firebaseConfig),
                 storageBucket: 'gs://petcare-6a561.appspot.com',
             });
-            return app;
+
         },
     },
     {
