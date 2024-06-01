606203fd5cdee330f78fdcb11ccdfd7f2c550380
PhucTruuong <133474302+PhucTruuong@users.noreply.github.com>
2024-06-01 10:31:44 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document, {
+  SwaggerModule.setup('api', app, document, {
     swaggerOptions: { defaultModelsExpandDepth: -1 },
   });
 
